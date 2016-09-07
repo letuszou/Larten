e58cd1421cdde86a1775b71c2b9734642dbb1ced
fragmentlayout <fragmentlayout@126.com>
2016-09-07 09:50:42 +0800

    add new style

--- app/src/main/java/com/panda/flashlight/activity/IndexActivity.java
+++ app/src/main/java/com/panda/flashlight/activity/IndexActivity.java
@@ -2,12 +2,18 @@ package com.panda.flashlight.activity;
 
 import android.hardware.Camera;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
+
 import com.panda.flashlight.R;
 import com.panda.flashlight.util.Config;
 
 import java.util.List;
+import java.util.Random;
 
 import cn.bmob.v3.Bmob;
 import cn.bmob.v3.update.BmobUpdateAgent;
@@ -17,6 +23,24 @@ public class IndexActivity extends BaseActivity implements View.OnClickListener 
     private ImageView iv_larten;
     private boolean isOpen;
     private Camera camera = null;
+    private RelativeLayout rl_bg;
+
+
+    private Handler mHandler = new Handler(new Handler.Callback() {
+        @Override
+        public boolean handleMessage(Message message) {
+            switch (message.what){
+                case 0:
+                    rl_bg.setBackgroundColor(getResources().getColor(R.color.gray));
+                    break;
+            }
+
+
+            return false;
+        }
+    });
+    private int y;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +50,7 @@ public class IndexActivity extends BaseActivity implements View.OnClickListener 
         initView();
     }
 
-    private void initBmob(){
+    private void initBmob() {
         Bmob.initialize(this, Config.Bmob_Api);
 //        BmobUpdateAgent.initAppVersion(this);
         BmobUpdateAgent.update(this);
@@ -34,6 +58,7 @@ public class IndexActivity extends BaseActivity implements View.OnClickListener 
 
     private void initView() {
         iv_larten = (ImageView) findViewById(R.id.iv_lantern);
+        rl_bg = (RelativeLayout) findViewById(R.id.rl_bg);
         iv_larten.setOnClickListener(this);
         isOpen = false;
         camera = Camera.open();
@@ -47,13 +72,20 @@ public class IndexActivity extends BaseActivity implements View.OnClickListener 
                 if (isOpen) {
                     turnLightOff(camera);
                     isOpen = false;
-                    iv_larten.setImageResource(R.drawable.close);
+                    iv_larten.setImageResource(R.drawable.new_close);
                 } else {
+                    setBackgroudColor(999);
                     turnLightOn(camera);
                     isOpen = true;
-                    iv_larten.setImageResource(R.drawable.open);
-
+                    iv_larten.setImageResource(R.drawable.new_open);
                 }
+            }
+        });
+        iv_larten.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+                mHandler.sendEmptyMessage(0);
+                return false;
             }
         });
 
@@ -67,7 +99,7 @@ public class IndexActivity extends BaseActivity implements View.OnClickListener 
         if (isOpen) {
             turnLightOff(camera);
             isOpen = false;
-            iv_larten.setImageResource(R.drawable.close);
+            iv_larten.setImageResource(R.drawable.new_close);
         }
 
     }
@@ -129,6 +161,35 @@ public class IndexActivity extends BaseActivity implements View.OnClickListener 
         }
 
     }
+
+
+    private void setBackgroudColor(int x) {
+        Random random = new Random();
+        y = random.nextInt(6);
+
+        switch (y) {
+            case 0:
+                rl_bg.setBackgroundColor(getResources().getColor(R.color.yellow));
+                break;
+            case 1:
+                rl_bg.setBackgroundColor(getResources().getColor(R.color.green));
+                break;
+            case 2:
+                rl_bg.setBackgroundColor(getResources().getColor(R.color.light));
+                break;
+            case 3:
+                rl_bg.setBackgroundColor(getResources().getColor(R.color.SkyBlue));
+                break;
+            case 4:
+                rl_bg.setBackgroundColor(getResources().getColor(R.color.Pink));
+                break;
+            case 5:
+                rl_bg.setBackgroundColor(getResources().getColor(R.color.LawnGreen));
+                break;
+        }
+
+    }
+
 }
 
 
